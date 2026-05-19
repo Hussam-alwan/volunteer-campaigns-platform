c06097d0d22b919fb6a60379aa62e94dc4c86e78
maram <lazour33@gmail.com>
2026-05-19 23:42:08 +0300

    test

--- src/main/java/com/uni/impact/ImpactApplication.java
+++ src/main/java/com/uni/impact/ImpactApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ImpactApplication {
+public class ImpactApplication
+{
 
     public static void main(final String[] args) {
         SpringApplication.run(ImpactApplication.class, args);
     }
 
 }
+//lll/
