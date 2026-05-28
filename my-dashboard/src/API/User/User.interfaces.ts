0b87411709d4d2eeb65a6e17762841746461b701
Mirnarihawi <97335587+Mirnarihawi@users.noreply.github.com>
2026-05-28 14:21:21 +0300

    Add User.interfaces.ts with IUser type definition

---
+++ my-dashboard/src/API/User/User.interfaces.ts
@@ -0,0 +1,12 @@
+interface IUser {
+  userId: number;
+  studentNumber: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  academicYear: number;
+  college: number;
+  isBanned: boolean;
+  createdAt: string;
+}
